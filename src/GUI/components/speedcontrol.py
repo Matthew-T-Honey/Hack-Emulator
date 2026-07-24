01a8e31c097c9388e81a92cbbac2b8e9556140b0
Matthew-T-Honey <matthewthoney@gmail.com>
2026-07-24 16:26:25 +0100

    Speed Linux Fix 1

--- src/GUI/components/speedcontrol.py
+++ src/GUI/components/speedcontrol.py
@@ -1,13 +1,13 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-import time
+import math
 from collections import deque
 
 class SpeedControl():
     def __init__(self, gui):
         self.gui = gui
-        self.widget = gui.ui.speed_widget
         self.speed_slider = gui.ui.speed_slider
         self.speed_label = gui.ui.speed_label
+        self.target_label = gui.ui.target_label
         
         self.times = deque()
         self.speed = 1
@@ -24,10 +24,14 @@ class SpeedControl():
             self.times.popleft()
         if len(self.times)>0:
             average_time = sum(self.times)/len(self.times)
-            self.speed_label.setText(str(round(average_time,2)))
+            if average_time >= 100:
+                self.speed_label.setText("Speed: "+str(int(average_time)))
+            else:
+                self.speed_label.setText("Speed: "+str(round(average_time,2)))
+            
 
     def reset_label(self):
-        self.speed_label.setText("0")
+        self.speed_label.setText("Speed: 0")
         self.times = deque()
         
     def update_speed(self):
@@ -35,4 +39,9 @@ class SpeedControl():
             self.speed = 10**((self.speed_slider.value()/10)-1)
         else:
             self.speed = 10**((self.speed_slider.value()/5)-2)
+        self.speed_label.setText("Speed: 0")
+        if self.speed >= 100:
+            self.target_label.setText("Target: "+str(int(self.speed)))
+        else:
+            self.target_label.setText("Target: "+str(round(self.speed,2)))
         self.times = deque()
